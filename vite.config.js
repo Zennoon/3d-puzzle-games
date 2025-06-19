2ca849d9b176485d433b55bd17b5a38f566dff09
Zennoon <98653096+Zennoon@users.noreply.github.com>
2025-06-19 01:48:48 -0700

    Update vite config file to include games files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import restart from 'vite-plugin-restart';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     root: 'src/',
@@ -14,7 +15,14 @@ export default defineConfig({
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                hanoi: resolve(__dirname, 'src/games/hanoi.html'),
+                maze: resolve(__dirname, 'src/games/maze.html')
+            }
+        }
     },
     plugins: [
         tailwindcss(),
